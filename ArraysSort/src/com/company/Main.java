95843975256be56173d9e00ceea0f9236ca4c140
Yevheniia778 <51268081+Yevheniia778@users.noreply.github.com>
2019-06-03 13:28:38 +0300

    ArraysSort

--- ArraysSort/src/com/company/Main.java
+++ ArraysSort/src/com/company/Main.java
@@ -11,19 +11,57 @@ public class Main {
         int size = input.nextInt();
         int a [] = new int[size];
         int b [] = new int[size];
-        System.out.print("Enter numbers for the first array: ");
+        System.out.print("Enter numbers for the first array: \n");
         for (int i = 0; i < size; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
             a[i] = input.nextInt();
         }
-        System.out.print("Enter numbers for the second array: ");
+        System.out.print("Enter numbers for the second array: \n");
         for (int j = 0; j < size; j++){
+            System.out.print("Enter number " + (j + 1) + ": ");
             b[j] = input.nextInt();
+        }
+
+        String l;
+        System.out.print("First array: ");
+        for (int i = 0; i < size; i++){
+            l = "" + a[i];
+            if (a[i] % 3 == 0) {
+             l += "!";
+            }
+            switch (a[i]) {
+                case 2:
+                case 7:
+                case 8:
+                case 15:
+                case 34:
+                    l += "#";
+                break;
+            }
+            System.out.print(l + " ");
+        }
+        System.out.print("\nSecond array: ");
+        for (int i = 0; i < size; i++){
+            l = "" + b[i];
+            if (b[i] % 3 == 0) {
+                l += "!";
+            }
+            switch (b[i]) {
+                case 2:
+                case 7:
+                case 8:
+                case 15:
+                case 34:
+                    l += "#";
+                    break;
+            }
+            System.out.print(l + " ");
         }
         int c [] = new int[size];
         for (int i = 0; i < size; i++){
             c[i] = a[i] * b[i];
         }
-        System.out.print("Result: ");
+        System.out.print("\nResult: ");
         for (int j = 0; j < c.length; j++){
             if (j < c.length - 1) {
                 System.out.print(c[j] + ",");
@@ -37,13 +75,13 @@ public class Main {
         for (int i = 0; i < c.length; i++){
             for (int j = i + 1; j < c.length; j++){
                 if (c[i] > c[j]) {
-                  temp = c[j];
-                  c[j] = c[i];
-                  c[i] = temp;
+                    temp = c[j];
+                    c[j] = c[i];
+                    c[i] = temp;
                 }
             }
         }
-        System.out.print("Result sort: ");
+        System.out.print("\nResult sort: ");
         for (int j = 0; j < c.length; j++){
             if (j < c.length - 1) {
                 System.out.print(c[j] + ",");
